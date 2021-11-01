d704c0fce802f60121d7f2c1e0a7571f0351f8cd
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2021-11-01 15:54:00 +0530

    Valid Triangle

---
+++ Valid Triangles.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n ,a ,b ,c;
+    cin >> n;
+    for(int i = 0 ; i < n ; ++i){
+        cin >> a >> b >>c;
+        if(a+b+c==180){
+            cout << "YES"<<endl;
+        }
+        else
+            cout << "NO"<<endl;
+    }
+    return 0;
+}
