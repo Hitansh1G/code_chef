5586b43528c67dfd3275bed1fba582851bb7b256
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2021-11-03 23:51:19 +0530

    games of wasseypur

---
+++ Games of Wasseypur.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+int main(){
+    int t,n;
+    cin >> t;
+    string s;
+    for(int i=0;i<t;i++){
+        cin >> n >> s;
+        int count=0
+        for(int j=0;j<n;j++){
+            if(s[j]==s[j++]){
+
+            }
+        }
+    }
+
+    return 0;
+}
