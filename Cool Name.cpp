dd628ce7c4b322fd71c0f5ed1944087a8b8e478b
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2021-11-03 23:50:01 +0530

    Cool Name

---
+++ Cool Name.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    while(n--){
+        string s;
+        cin >> s;
+        long long int n = 0;
+        sort(s.begin(),s.end());
+        for(int i = 0;i<s.size();i++)
+        n+=((s[i]-'a')+1)*(i+1);
+        cout<<n<<endl;
+    }
+    return 0;
+}
