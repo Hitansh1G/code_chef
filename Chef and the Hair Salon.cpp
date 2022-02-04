c888b3cd9ab3b686fbf29033234e8b0c75d74144
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2022-02-04 19:06:37 +0530

    Chef and the Hair Salon

---
+++ Chef and the Hair Salon.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<map>
+#include<set>
+#include<list>
+#include<queue>
+#include<stack>
+#include<cstring>
+#include<cmath>
+#include<cstdlib>
+#include<unordered_map>
+#include<unordered_set>
+#define Fast ios_base::sync_with_stdio(false); cin.tie(NULL);
+using namespace std;
+int main(){
+    Fast
+    int t,a,b,p;
+    cin >> t;
+    for(int i=0 ; i<t ; i++){
+        cin >> a>>b;
+        cout<<a*b<<endl;
+    }
+    return 0;
+}
