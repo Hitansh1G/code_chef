862b6fa3041e86738ca3586202322e86fc2ac8b3
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2022-03-09 20:02:45 +0530

    Fill the Bucket

---
+++ easy problems/Ones Guessing.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<map>
+#include<set>
+#include<list>
+#include<queue>
+#include<stack>
+#include<cstring>
+#include<cmath>
+#include<cstdlib>
+#include<unordered_map>
+#include<unordered_set>
+#define Fast ios_base::sync_with_stdio(false); cin.tie(NULL);
+typedef long long ll;
+using namespace std;
+int main(){
+    Fast
+    int n;
+    cin >> n;
+    while(n--){
+        
+    }
+    return 0;
+}
+
+/*
+binary string of len n
+k 1s
+n-k 0s
+need to take n guesses on the value of k 
+after the ith guess 
+*/
