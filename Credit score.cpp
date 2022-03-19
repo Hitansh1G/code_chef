afee4bccd0674126f4e6d44ea8bcc46b1dcbdc1e
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2022-03-19 20:04:02 +0530

    Credit score

---
+++ Credit score.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<map>
+#include<set>
+#include<list>
+#include<queue>
+#include<stack>
+#include<cstring>
+#include<cmath>
+#include<cstdlib>
+#include<unordered_map>
+#include<unordered_set>
+#define Fast ios_base::sync_with_stdio(false); cin.tie(NULL);
+typedef long long ll;
+using namespace std;
+int main(){
+    Fast
+    int n;
+    cin >> n;
+    if(n>=750) cout<<"YES";
+    else cout<<"NO";
+    return 0;
+}
