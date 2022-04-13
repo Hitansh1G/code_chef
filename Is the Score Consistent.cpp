f73c280ce7d82d05b599a47a44c7cdddb24d0723
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2022-04-13 20:06:52 +0530

    Is the Score Consistent

---
+++ Is the Score Consistent.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<map>
+#include<set>
+#include<list>
+#include<queue>
+#include<stack>
+#include<cstring>
+#include<cmath>
+#include<cstdlib>
+#include<unordered_map>
+#include<unordered_set>
+#define Fast ios_base::sync_with_stdio(false); cin.tie(NULL);
+typedef long long ll;
+using namespace std;
+int main(){
+    Fast
+    int t;
+    cin >> t; 
+    while(t--){
+        int a,b,c,d;
+        cin >> a >> b;
+        cin >> c >> d;
+        if(c>=a && d>=b){
+            cout<<"POSSIBLE"<<endl;
+        }
+        else cout << "IMPOSSIBLE"<<endl;
+    }
+    return 0;
+}
