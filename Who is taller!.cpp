de2f807bed85940ed82bb4631e672f4068eb0fcf
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2022-04-27 20:03:34 +0530

    Who is taller

---
+++ Who is taller!.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<map>
+#include<set>
+#include<list>
+#include<queue>
+#include<stack>
+#include<cstring>
+#include<cmath>
+#include<cstdlib>
+#include<unordered_map>
+#include<unordered_set>
+#define Fast ios_base::sync_with_stdio(false); cin.tie(NULL);
+typedef long long ll;
+using namespace std;
+int main(){
+    Fast
+    int t;
+    cin >> t;
+    while(t--){
+        int a,b;
+        cin >> a >> b;
+        if(a>b)cout<<"A"<<endl;
+        if(b>a)cout<<"B"<<endl;
+        
+    }    
+    return 0;
+}
