b258170009f744e7b1075ce829db2b351a3e3904
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2022-05-01 21:03:18 +0530

    Game of Pooks

---
+++ Game of Pooks.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<map>
+#include<set>
+#include<list>
+#include<queue>
+#include<stack>
+#include<cstring>
+#include<cmath>
+#include<cstdlib>
+#include<unordered_map>
+#include<unordered_set>
+#define Fast ios_base::sync_with_stdio(false); cin.tie(NULL);
+typedef long long ll;
+using namespace std;
+int main(){
+    Fast
+    int t;
+    cin >> t; 
+    while(t--){
+        int x;
+        cin >> x;
+        if(x==0)cout<<0<<endl;
+        else if(x==1)cout<<1<<endl;
+        else if(x==2)cout<<1<<endl;
+        else if(x==3)cout<<2<<endl;
+        else cout<<x<<endl;
+
+
+    }
+    return 0;
+}
