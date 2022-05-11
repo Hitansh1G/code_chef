4fcdd1bb831e5b0a61913564da4abb001c4c7329
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2022-05-11 20:04:25 +0530

    Chef gives Party

---
+++ Chef gives Party.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<map>
+#include<set>
+#include<list>
+#include<queue>
+#include<stack>
+#include<cstring>
+#include<cmath>
+#include<cstdlib>
+#include<unordered_map>
+#include<unordered_set>
+#define Fast ios_base::sync_with_stdio(false); cin.tie(NULL);
+typedef long long ll;
+using namespace std;
+int main(){
+    Fast
+    int t;
+    cin >> t;
+    while(t--){
+        int n,x,k;
+        cin >> n >> x >> k;
+        if(n*x>k)cout<< "NO"<<endl;
+        else cout<<"YES"<<endl;
+    }
+    return 0;
+}
