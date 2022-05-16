3b084e86e59ce5806367f2a42d84667b49e53fc6
Hitansh1G <86662038+Hitansh1G@users.noreply.github.com>
2022-05-16 09:58:25 +0530

    Maximum Pairwise Modular Sum

---
+++ Maximum Pairwise Modular Sum.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#include<vector>
+#include<map>
+#include<set>
+#include<list>
+#include<queue>
+#include<stack>
+#include<cstring>
+#include<cmath>
+#include<cstdlib>
+#include<unordered_map>
+#include<unordered_set>
+#define Fast ios_base::sync_with_stdio(false); cin.tie(NULL);
+typedef long long ll;
+using namespace std;
+int main(){
+    Fast
+    int t;
+    cin >> t;
+    while(t--){
+        int n, m; cin >> n >> m;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        sort(a.begin(), a.end());
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans = max(ans, a[i] + a[n - 1] + ((a[i] - a[n - 1]) % m + m) % m);
+        }
+        cout << ans <<endl;
+
+    }
+    return 0;
+}
